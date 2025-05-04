e81f8c9953ba6207e93e88fdbd5e0bf564cf1b8f
Jefry Afrio <jefryafrio3@gmail.com>
2025-05-04 16:06:53 +0700

    Menambahkan struktur awal dan state dasar dengan ref

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_course = ref('')
+const input_task = ref('')
+const input_category = ref(null)
+const input_deadline = ref('')
+
+const showOnlyUndone = ref(false)
+
+const courseOptions = [
+  'Desain dan Analis Algoritma',
+  'Elektronika Digital',
+  'Grafika Komputer',
+  'Jaringan Komputer',
+  'Pemograman Berorientasi Objek',
+  'Pemograman Berbasis Komponen',
+  'Matematika Diskrit',
+  'Interkasi Manusia dan Komputer',
+]
+</script>
+
+<template>
+  
+</template>

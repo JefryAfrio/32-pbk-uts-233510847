fc37bdd37ea5a1976d60a6d25c654c4f1209b381
Jefry Afrio <jefryafrio3@gmail.com>
2025-05-04 16:28:37 +0700

    Menambahkan tampilan form input tugas

--- src/App.vue
+++ src/App.vue
@@ -76,5 +76,46 @@ const filteredTodos = computed(() => {
 </script>
 
 <template>
+  <div class="container">
+    <main class="app">
+      <section class="greeting">
+        <h2 class="title">Selamat Datang Dr.Jefry</h2>
+      </section>
 
+      <section class="create-todo">
+        <h3>Ada Tugas Apa Hari Ini?</h3>
+
+        <form @submit.prevent="addTodo">
+          <h4>Mata Kuliah</h4>
+          <select v-model="input_course">
+            <option value="" disabled selected>Pilih Mata Kuliah</option>
+            <option v-for="course in courseOptions" :key="course" :value="course">{{ course }}</option>
+          </select>
+
+          <h4>Tugas / Materi</h4>
+          <input type="text" placeholder="Contoh: P8 - Membuat Sistem Todo List" v-model="input_task" />
+
+          <h4>Kategori</h4>
+          <div class="options">
+            <label>
+              <input type="radio" name="category" value="teori" v-model="input_category" />
+              <span class="bubble teori"></span>
+              <div>Teori</div>
+            </label>
+
+            <label>
+              <input type="radio" name="category" value="pratikum" v-model="input_category" />
+              <span class="bubble pratikum"></span>
+              <div>Pratikum</div>
+            </label>
+          </div>
+
+          <h4>Batas Waktu</h4>
+          <input type="date" v-model="input_deadline" />
+
+          <input type="submit" value="Tambahkan" />
+        </form>
+      </section>
+      </main>
+      </div>
 </template>
